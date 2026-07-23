6b23427cc16c35f6d6390e0a666b8d8d3730d474
mattiaippoliti1 <81828656+m-ippo@users.noreply.github.com>
2026-07-23 11:52:55 +0200

    Risoluzione problemi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from "react";
-import {motion, AnimatePresence} from "framer-motion";
+import {motion} from "framer-motion";
 
 // Per inserire immagini
 import backgroundImage from "./assets/sfondo-hb2026.webp";
